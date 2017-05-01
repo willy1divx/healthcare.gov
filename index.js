1684cda28f232a98d56e4de7343d33e19c7d6faf
williamd <willy1divx@yahoo.com>
2017-05-01 18:39:17 +0000

    dynamically created everything using just plain javascript

--- index.js
+++ index.js
@@ -1,14 +1,65 @@
+
 	var family = [];
 	var server = ["to server:"];
 	var number = 0;
 	var reduceNumber = 0;
 	var filtered = [];
 
-  
+  	var style = document.getElementsByTagName('style')[0].innerHTML=".debug {font-family: monospace;border: 1px solid black;padding: 10px;overflow:auto;}";
+  	var form = document.getElementsByTagName('form')[0].setAttribute("id", "name-form");
+  	var inputAge = document.getElementsByTagName('input')[0].setAttribute("type", "number");
+	var inputSmoker = document.getElementsByTagName('input')[1].setAttribute("type", "radio");
+	document.getElementsByTagName('input')[1].setAttribute("name", "smoker");
+	document.getElementsByTagName('input')[1].value = "smoker";
+	var divSmoker = document.getElementsByTagName('div')[3];
+	var labelNonsmoker = document.createElement('label');
+	labelNonsmoker.innerHTML = "Nonsmoker?";
+	var inputNonsmoker = document.createElement('input');
+	labelNonsmoker.appendChild(inputNonsmoker);
+	divSmoker.appendChild(labelNonsmoker);
+	document.getElementsByTagName('input')[2].setAttribute("type", "radio");
+	document.getElementsByTagName('input')[2].setAttribute("name", "smoker");
+	document.getElementsByTagName('input')[2].value = "nonsmoker";
+	var addButton = document.getElementsByTagName('button')[0];
+	addButton.onclick = function (){
+		form = document.getElementById("name-form");
+			 form.onsubmit = function(e){
+			 	e.preventDefault();
+			     
+				var age = document.getElementsByName("age")[0].value;
+			  	var rel = document.getElementsByName("rel")[0].value;
+			  	var smoker = (form.smoker.value);
+				
 
-	
+				 if ( rel !== "" && age > 0 && age !== ""){
+				  	json = {
+						"age": age,
+						"relationship":rel,
+						"smoker":smoker
+					};
 
-	var removeFamily = function (){
+			
+					family.push(json);	
+				    var node = document.createElement("LI");
+				    node.id = number;
+				    var familyMember = JSON.stringify(json);            
+					var textnode = document.createTextNode(familyMember);        
+					node.appendChild(textnode);                             
+					var list = document.getElementsByClassName("household");
+					list[0].appendChild(node);  
+					number++;  
+					return family;
+				}
+			};
+	};
+
+	var deleteButton = document.createElement('button');
+	var addRemove = document.getElementsByTagName('div')[4];
+	addRemove.appendChild(deleteButton);
+	document.getElementsByTagName('button')[1].setAttribute("class", "remove");
+	document.getElementsByTagName('button')[1].innerHTML = "remove";
+	var deletePerson = document.getElementsByTagName('button')[1];
+	deletePerson.onclick = function () {
 		filtered = [];
 		form = document.getElementById("name-form");
 		form.onsubmit = function(e){
@@ -27,56 +78,23 @@
 				}
 				family = filtered;
 				
-		}
+		};
 	};
 
-	var addFamily = function (){
-		    form = document.getElementById("name-form");
-			 form.onsubmit = function(e){
-			 	e.preventDefault();
-			     
-				var age = document.getElementsByName("age")[0].value;
-			  	var rel = document.getElementsByName("rel")[0].value;
-			  	var smoker = (form.smoker.value);
-				
-
-				 if ( rel != "" && age > 0 && age != ""){
-				  	json = {
-						"age": age,
-						"relationship":rel,
-						"smoker":smoker
-					};
-
-			
-					family.push(json);
-				// console.log("william" + family[0].rel);
-					
-				    var node = document.createElement("LI");
-				    node.id = number;
-				    var familyMember = JSON.stringify(json);            
-					var textnode = document.createTextNode(familyMember);        
-					node.appendChild(textnode);                             
-					var list = document.getElementsByClassName("household");
-					list[0].appendChild(node);  
-					number++  
-					return family;
-				}
-			}
-	};
-	
-	function toServer(){
-		console.log(filtered);
+	var submitButton = document.getElementsByTagName('button')[2];
+	submitButton.onclick = function (){
+			console.log(filtered);
 		if (filtered.length > 0){
 			family = filtered;
 		}
-
+		console.log(family);
 		var form = document.getElementById("name-form");
 		form.onsubmit = function(e){
 		e.preventDefault();
 		var age = document.getElementsByName("age")[0].value;
 	  	var rel = document.getElementsByName("rel")[0].value;
 	  	var smoker = (form.smoker.value);
-		 if ( rel != "" && age > 0 && age != ""){
+		 if ( rel !== "" && age > 0 && age !== ""){
 			server.push(family);
 			console.log(server);
 			var familyMember = JSON.stringify(server);
@@ -86,7 +104,6 @@
 			show[0].innerHTML = familyMember;
 			family = [];
 		}
-		
 		filtered = [];
 		var myNode = document.getElementsByClassName("household");
 		while (myNode[0].firstChild) {
@@ -95,11 +112,15 @@
 		number = 0;
 		reduceNumber = 0;
 		return server;
-		}
-		}
+		};
+	};
 
-		var retrieve = function (){
-			var form = document.getElementById("name-form");
+	var retrieveButton = document.createElement('button');
+	var serverDiv = document.getElementsByTagName('div')[5];
+	serverDiv.appendChild(retrieveButton);
+	retrieveButtonText = document.getElementsByTagName('button')[3].innerHTML = "retrieve last";
+	retrieveButton.onclick = function () {
+		var form = document.getElementById("name-form");
 			 form.onsubmit = function(e){
 			 	e.preventDefault();
 			 	number = 0;
@@ -116,15 +137,8 @@
 					list[0].appendChild(node);  
 					number++ 
 				}
-				console.log(family[0]);
-				console.log(family[1]);
-				console.log(family[2]);
 			} 
-		}
-
-
+		};
 
 
 	
-
-
